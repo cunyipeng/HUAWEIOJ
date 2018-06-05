1e4e1d77e5169ffc8caf1ca1e55bbf3361cac59e
flypyCode <job_cunyipeng@163.com>
2018-06-05 16:25:48 +0800

    Update 海马的神灯.c

--- 海马的神灯.c
+++ 海马的神灯.c
@@ -8,8 +8,8 @@
 bool lightStatus(int n)
 {
     double tmp1 = sqrt((double)n);
-	double tmp2 = (int)tmp1;
-	if(tmp1 == tmp2)
-		return false;
+    double tmp2 = (int)tmp1;
+    if(tmp1 == tmp2)
+	return false;
     return true;
 }
