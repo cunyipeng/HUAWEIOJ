171763764ba6426936b6e5c27c8c13adb747b5c0
flypyCode <job_cunyipeng@163.com>
2018-06-05 16:26:53 +0800

    Create 字符个数统计.c

---
+++ 字符个数统计.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "oj.h"
+
+
+/*
+功能:
+
+输入:字符串
+
+输出:无
+     
+返回:字符个数
+*/
+
+int GetCount( char* strInValue )
+{
+	 if(strInValue == NULL)
+		  return 0;
+
+	 int map[127] = {0};
+	 int count = 0;
+	 while(*strInValue != '\0')
+	 {
+		  map[*strInValue]++;
+         strInValue++;
+   }
+	 for(int i = 0; i < 127; i++)
+	 {
+		  if(map[i])
+			   count++;
+	 }  
+	 return count;
+}
